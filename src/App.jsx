17c7f33d2ac9f12652c14fec413c013d5edd34c5
zalish-shawon <zalish.shawon@gmail.com>
2023-12-30 00:31:00 +0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 
 function App() {
   
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/about' element={<About></About>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/signIn' element={<SignIn/>} />
+        <Route path='/signUp' element={<SignUp/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
