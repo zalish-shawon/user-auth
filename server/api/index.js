da24818ed13a003707cfcfb7bfe423676211798c
zalish-shawon <zalish.shawon@gmail.com>
2023-12-30 02:56:40 +0600

    controllers added

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import userRoutes from './routes/user.routes.js'
 dotenv.config();
 
 mongoose.connect('mongodb+srv://zalishmahmud6:GoBq3fJbeRsi7td2@mern.swtv2jg.mongodb.net/user-auth?retryWrites=true&w=majority')
@@ -19,6 +20,4 @@ app.listen(3000, ()=> {
     console.log("Server listening on port 3000");
 })
 
-app.get('/', (req, res) => {
-    res.send('User auth server is running')
-})
+app.use('/api/user',userRoutes)
