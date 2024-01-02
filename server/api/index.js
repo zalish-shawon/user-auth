5bf0f897b19c13e6016a7d892c1b49429c4e516c
zalish-shawon <zalish.shawon@gmail.com>
2024-01-03 02:13:46 +0600

    added

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
 dotenv.config();
 
 mongoose.connect('mongodb+srv://zalishmahmud6:GoBq3fJbeRsi7td2@mern.swtv2jg.mongodb.net/user-auth?retryWrites=true&w=majority')
@@ -15,9 +16,11 @@ mongoose.connect('mongodb+srv://zalishmahmud6:GoBq3fJbeRsi7td2@mern.swtv2jg.mong
 
 const app = express();
 
+app.use(express.json());
 
 app.listen(3000, ()=> {
     console.log("Server listening on port 3000");
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth', authRoutes)
--- server/api/routes/user.routes.js
+++ server/api/routes/user.routes.js
@@ -6,3 +6,5 @@ const router = express.Router();
 router.get('/', test)
 
 export default router;
+
+
